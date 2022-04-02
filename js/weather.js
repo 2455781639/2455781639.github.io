53a72708cb95158db56c1cd79e857f8b60cbd441
2455781639 <2455781639@qq.com>
2022-04-02 20:08:09 +0800

    Site updated: 2022-04-02 20:08:09

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "125",
+    "top": "20",
+    "key": "97b29dae1afa481689e13d1349f4a8ad"
+  }
+}
