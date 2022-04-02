3fd52766b277bc0b9b378497a18a5e555a40806c
2455781639 <2455781639@qq.com>
2022-04-02 20:13:22 +0800

    Site updated: 2022-04-02 20:13:22

--- js/weather.js
+++ js/weather.js
@@ -2,22 +2,23 @@ WIDGET = {
   "CONFIG": {
     "modules": "01234",
     "background": "1",
-    "tmpColor": "FFFFFF",
+    "tmpColor": "FF0000",
     "tmpSize": "16",
-    "cityColor": "FFFFFF",
+    "cityColor": "FFFF00",
     "citySize": "16",
-    "aqiColor": "FFFFFF",
+    "aqiColor": "FF00FF",
     "aqiSize": "16",
     "weatherIconSize": "24",
     "alertIconSize": "18",
     "padding": "10px 10px 10px 10px",
-    "shadow": "0",
+    "shadow": "1",
     "language": "auto",
+    "borderRadius": "10",
     "fixed": "true",
     "vertical": "top",
     "horizontal": "left",
-    "left": "125",
-    "top": "20",
-    "key": "97b29dae1afa481689e13d1349f4a8ad"
+    "left": "130",
+    "top": "10",
+    "key": "d2df5979b6314086b323ed6ec16e27a9"
   }
 }
